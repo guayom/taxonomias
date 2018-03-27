0451dfd631bb19ae231bae339e3af89ea73ca434
guayom <guayo.mena@gmail.com>
2018-03-27 13:45:59 -0600

    Finish script to rename files

--- import/rename.js
+++ import/rename.js
@@ -1,8 +1,22 @@
-const testFolder = './content/especies/';
+const folder = './content/especies/';
 const fs = require('fs');
+var parser = require('parser-front-matter');
+var slug = require('slug')
 
-fs.readdir(testFolder, (err, files) => {
+fs.readdir(folder, (err, files) => {
   files.forEach(file => {
-    console.log(file);
+    if (file != "_index.md") {
+      fs.readFile("./content/especies/"+file, (err, data) => {
+        if (err) throw err;
+        var parsedFile = { contents: data };
+        parser.parse(parsedFile, function (err, res) {
+          let newTitle = slug(res.data.nombre_cientifico, { lower: true }) + ".md"
+          fs.rename('./content/especies/' + file, './content/especies/' + newTitle, function (err) {
+            if (err) console.log('ERROR: ' + err);
+          });
+          console.log(`${file} --- ${newTitle}`)
+        });
+      });
+    }
   });
 })
